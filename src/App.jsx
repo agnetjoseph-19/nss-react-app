a9029bd30e2392f0e0ee9e1f216eb35245e48658
agnetjoseph@gmail.com <agnetjoseph@gmail.com>
2026-07-15 01:14:55 +0530

    add completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import AddVol from './AddVol'
+import AddVol from './components/AddVol'
 
 function App() {
   const [count, setCount] = useState(0)
